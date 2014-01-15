fd9b3a9596d4fed3e9a87aae2efafadf71aace50
Moiri Gamboni <moiri.gamboni@nyu.edu>
2014-01-15 10:13:31 +0400

    Scrapy project created

---
+++ scraper/diet/diet/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for diet project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'diet'
+
+SPIDER_MODULES = ['diet.spiders']
+NEWSPIDER_MODULE = 'diet.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'diet (+http://www.yourdomain.com)'
