ba75c0a6dc817334c992a02ef2c877bc5fe21338
Moiri Gamboni <moiri.gamboni@nyu.edu>
2014-01-20 01:40:09 +0400

    Fixed USDA db, updated manual_parser

--- recipe-parser/manual_parser.py
+++ recipe-parser/manual_parser.py
@@ -1,16 +1,18 @@
-import json, csv
+import json, csv, sys
 usda = None
 
 def load_usda():
 	global usda
-	with open('cleanFoodData.json') as f:
-	  usda = json.load(f)
+	usda = []
+	with open('usda_food_db.json') as f:
+		for line in f:
+			usda.append(json.loads(line))
 
 def get_usda_ingredient(id):
 	global usda
-	for food in usda:
-		if food["id"] == int(id):
-			return food
+	for ingredient in usda:
+		if ingredient["meta"]["ndb_no"] == int(id):
+			return ingredient
 	return None
 """
 recipe =
@@ -58,23 +60,23 @@ def save_recipe(recipe):
 	for ingredient in recipe["ingredients"]:
 		usda_ingredient = get_usda_ingredient(ingredient["id"])
 		ingredient["nutrients"] = usda_ingredient["nutrients"]
-		ingredient["description"] = usda_ingredient["description"]
+		ingredient["description"] = usda_ingredient["name"]["long"]
 		if int(ingredient["portion"]) == 0:
-			ingredient["portion"] = {"grams":100, "unit":"100g"}
+			ingredient["portion"] = {"g":100, "unit":"100g"}
 		else:
 			ingredient["portion"] = usda_ingredient["portions"][int(ingredient["portion"])-1]
 		for nutrient in ingredient["nutrients"]:
-			if nutrient["description"] not in nutrients:
-				nutrients.append(nutrient["description"])
-				computed_nutrients[nutrient["description"]] = []
+			if nutrient["name"] not in nutrients:
+				nutrients.append(nutrient["name"])
+				computed_nutrients[nutrient["name"]] = []
 		ids.append(ingredient["id"])
 		descriptions.append(ingredient["description"])
 		portions.append(ingredient["portion"]["unit"])
 		quantities.append(ingredient["quantity"])
 	for i,ingredient in enumerate(recipe["ingredients"]):
 		for nutrient in ingredient["nutrients"]:
-			if len(computed_nutrients[nutrient["description"]]) == i:
-				computed_nutrients[nutrient["description"]].append(float(nutrient["value"])*float(ingredient["quantity"])*float(ingredient["portion"]["grams"])/float(100))
+			if len(computed_nutrients[nutrient["name"]]) == i:
+				computed_nutrients[nutrient["name"]].append(float(nutrient["value"])*float(ingredient["quantity"])*float(ingredient["portion"]["g"])/float(100))
 			else:
 				continue
 		for nutrient_desc in computed_nutrients:
@@ -82,13 +84,13 @@ def save_recipe(recipe):
 				computed_nutrients[nutrient_desc].append(0)
 
 	with open('manual/'+recipe["title"]+'.csv','wb+') as f:
-	  writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-	  writer.writerow(['Ingredient ID']+ids)
-	  writer.writerow(['Ingredient Description']+descriptions)
-	  writer.writerow(['Portion size']+portions)
-	  writer.writerow(['Quantity']+quantities)
-	  for nutrient_desc, nutrient_values in computed_nutrients.iteritems():
-	  	writer.writerow([nutrient_desc]+nutrient_values+[reduce(lambda x, y: x+y, nutrient_values)])
+		writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+		writer.writerow(['Ingredient ID']+ids)
+		writer.writerow(['Ingredient Description']+descriptions)
+		writer.writerow(['Portion size']+portions)
+		writer.writerow(['Quantity']+quantities)
+		for nutrient_desc, nutrient_values in computed_nutrients.iteritems():
+			writer.writerow([nutrient_desc]+nutrient_values+[reduce(lambda x, y: x+y, nutrient_values)])
 
 def main():
 	load_usda()
@@ -100,15 +102,21 @@ def main():
 				print "Ingredient not found!"
 				continue
 			else:
-				ingredient = {"id":usda_ingredient["id"]}
+				ingredient = {"id":usda_ingredient["meta"]["ndb_no"]}
 				print json.dumps(usda_ingredient["portions"], sort_keys=True, indent=4, separators=(',', ': '))
-				ingredient["portion"] = input("Please enter portion type (seen in 'amount' property, 0 for default 100g)\n")
+				ingredient["portion"] = input("Please enter portion type (0 for default 100g)\n")
 				ingredient["quantity"] = input("Please enter quantity (i.e. quantity*portion_type = total grams)\n")
 				recipe["ingredients"].append(ingredient)
-				if raw_input("Type enter to add another ingredient or 'n' to start a new recipe\n") == "n":
+				o = raw_input("Type enter to add another ingredient, 'n' to start a new recipe, 'q' to save and quit\n")
+				if o == "n":
 					save_recipe(recipe)
 					break
+				elif o = "q":
+					save_recipe(recipe)
+					sys.exit("Quit")
 				else:
 					continue
-main()
+
+if __name__=="__main__":
+	main()
 
