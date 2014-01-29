5620ffe1625b3659e0195175b2ffa4b82053e14e
Moiri Gamboni <moiri.gamboni@nyu.edu>
2014-01-29 07:33:08 -0500

    TextBlob and NLTK POS tagging for noun extraction

--- recipe-parser/ingredient_matching.py
+++ recipe-parser/ingredient_matching.py
@@ -4,39 +4,31 @@ usda = None
 def load_usda():
 	global usda
 	usda = []
-	split_usda = []
-	with open('splitUSDA.json') as f:
+	with open('nounUSDA.json') as f:
 		for line in f:
 			usda.append(json.loads(line))
 
-def get_nouns(ingredient_name):
-	text = nltk.word_tokenize(ingredient_name)
-	tagged_name = nltk.pos_tag(text)
-	nouns = [word for (word, tag) in tagged_name if tag in ["NN","NNP","NNPS","NNS","NP"]]
-	if len(nouns) == 0:
-		nouns = [word for (word, tag) in tagged_name if tag == "JJ"]
-	if len(nouns) == 0:
-		nouns = [word for (word, tag) in tagged_name if tag == ["RB","VBN","DT"]]
-	if len(nouns) == 0:
-		reg = re.compile(r"[\w']+")
-		nouns = reg.findall(ingredient_name)
-	return nouns
-
-def get_ratios(ingredient_name, db):
+def get_ratios(ing_name, db):
 	reg = re.compile(r"[\w']+")
 	ratios = {}
-	ingredient_name_split = reg.findall(ingredient_name.lower())
+	ing_split = reg.findall(ing_name.lower())
 	for ingredient in db:
 		usda_name = ingredient["name"]["long"].lower()
-		usda_name_split = reg.findall(usda_name)
-		ratios[usda_name] = []
-		for ingredient_word in ingredient_name_split:
-			ratios[usda_name].append([difflib.SequenceMatcher(None, ingredient_word, usda_word).ratio() for usda_word in usda_name_split])
+		usda_split = []
+		for word in reg.findall(usda_name):
+			if word in ingredient["name"]["split"]:
+				usda_split.append(word)
+		if usda_split == []:
+			usda_split = reg.findall(usda_name)
+		ratio_list = [((ing_word, usda_word), difflib.SequenceMatcher(None, ing_word, usda_word).ratio()) for usda_word in usda_split for ing_word in ing_split]
+		ratios[usda_name] = {key: value for (key, value) in ratio_list}
 	return ratios
+
+def get_max_ratio(ingredient
 
 def get_bests(ratios):
 	matches = (0,[])
-	for ingredient, ratio_list in ratios.iteritems():
+	for ingredient, ratio_dict in ratios.iteritems():
 		m = max([max(ratios) for ratios in ratio_list])
 		if m > matches[0]:
 			matches = (m, [ingredient])
@@ -132,62 +124,55 @@ def main():
 	ingredients = []
 	with open('testRecipe.json') as f:
 		ingredients = json.load(f)["ingredients_no_quantities_no_units"]
-	second = False
-	for ingredient in ingredients:
-		if second:
-			ratios = get_ratios(ingredient, usda)
-			print ingredient
-			name = "spinach, raw"
-			match = []
+	names = {"large flour tortillas":"tortillas, ready-to-bake or -fry, flour, shelf stable","fresh lime juice":"lime juice, raw","small avocado, stone removed, peeled, mashed":"avocados, raw, all commercial varieties","coarsely grated vintage cheddar":"cheese, cheddar","baby spinach leaves":"spinach, raw","Greek-style feta, crumbled":"cheese, feta","smoked chicken breast fillet, thinly sliced":"chicken, broiler or fryers, breast, skinless, boneless, meat only, cooked, grilled"}
+	for i, ingredient in enumerate(ingredients):
+		ratios = get_ratios(ingredient, usda)
+		print ingredient
+		match = []
 
-			match.append(sorted(get_bests(ratios)[1]))
+		match.append((sorted(get_bests(ratios)[1]), "best", ""))
 
-			match.append(sorted(get_max_averages(ratios)[1]))
+		match.append((sorted(get_max_averages(ratios)[1]), "average", ""))
 
-			threshold = 1.0
+		threshold = 1.0
+		tmp_match = sorted(get_averages_above_threshold(ratios,threshold)[1])
+		while all(map(lambda name: name not in tmp_match, [name for orig, name in names.iteritems()])) and threshold != 0:
+			threshold -= 0.25
 			tmp_match = sorted(get_averages_above_threshold(ratios,threshold)[1])
-			while name not in tmp_match and threshold != 0:
-				threshold -= 0.25
-				tmp_match = sorted(get_averages_above_threshold(ratios,threshold)[1])
-			match.append(tmp_match)
-			print(threshold)
+		match.append((tmp_match, "threshold average", threshold))
 
-			threshold = 1.0
+		threshold = 1.0
+		tmp_match = sorted(get_bests_above_threshold(ratios,threshold)[1])
+		while all(map(lambda name: name not in tmp_match, [name for orig, name in names.iteritems()])) and threshold != 0:
+			threshold -= 0.25
 			tmp_match = sorted(get_bests_above_threshold(ratios,threshold)[1])
-			while name not in tmp_match and threshold != 0:
-				threshold -= 0.25
-				tmp_match = sorted(get_bests_above_threshold(ratios,threshold)[1])
-			match.append(tmp_match)
-			print(threshold)
+		match.append((tmp_match, "threshold max", threshold))
 
-			match.append(sorted(get_weighted_bests(ratios)[1]))
+		match.append((sorted(get_weighted_bests(ratios)[1]), "weighted max", ""))
 
-			match.append(sorted(get_weighted_max_averages(ratios)[1]))
+		match.append((sorted(get_weighted_max_averages(ratios)[1]), "weighted average", ""))
 
-			threshold = 1.0
+		threshold = 1.0
+		tmp_match = sorted(get_weighted_bests_above_threshold(ratios,threshold)[1])
+		while all(map(lambda name: name not in tmp_match, [name for orig, name in names.iteritems()])) and threshold != 0:
+			threshold -= 0.25
 			tmp_match = sorted(get_weighted_bests_above_threshold(ratios,threshold)[1])
-			while name not in tmp_match and threshold != 0:
-				threshold -= 0.25
-				tmp_match = sorted(get_weighted_bests_above_threshold(ratios,threshold)[1])
-			match.append(tmp_match)
-			print(threshold)
+		match.append((tmp_match, "weighted threshold best", threshold))
 
-			threshold = 1.0
+		threshold = 1.0
+		tmp_match = sorted(get_weighted_averages_above_threshold(ratios,0.5)[1])
+		while all(map(lambda name: name not in tmp_match, [name for orig, name in names.iteritems()])) and threshold != 0:
+			threshold -= 0.25
 			tmp_match = sorted(get_weighted_averages_above_threshold(ratios,0.5)[1])
-			while name not in tmp_match and threshold != 0:
-				threshold -= 0.25
-				tmp_match = sorted(get_weighted_averages_above_threshold(ratios,0.5)[1])
-			match.append(tmp_match)
-			print(threshold)
+		match.append((tmp_match, "weighted threshold average", threshold))
 
-			for matched in match:
-				print len(matched)
-				print json.dumps(matched,indent=4)
-				print '-'*80
-			print '*'*80
-			raw_input()
-		else:
-			second = True
+		for matched in match:
+			if any(map(lambda name: name in matched[0], [name for orig, name in names.iteritems()])):
+				print "method: "+matched[1]
+				print "length: "+str(len(matched[0]))
+				print "threshold: "+str(matched[2])
+				print ""
+		print '>'*80
 
 
 
---
+++ recipe-parser/usda_nouns.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+from nltk import pos_tag, word_tokenize
+import textblob, json
+text = []
+usda = {}
+with open('reducedUSDA.json') as f:
+	for line in f:
+		ingredient = json.loads(line)
+		usda[ingredient["name"]["long"]] = ingredient
+
+translation = {}
+print("Getting text")
+for ingredient in usda:
+	text.append(ingredient)
+	text.append(ingredient.lower())
+	text.append("I eat "+ingredient+".")
+	text.append("I eat "+ingredient.lower()+".")
+	text.append(ingredient+" tastes good.")
+	text.append(ingredient.lower()+" tastes good.")
+	translation[ingredient.lower()] = ingredient
+
+tags = []
+done = 0.0
+print("Getting tags")
+for phrase in text:
+	tags.append((phrase, "NLTK", [tag[0].lower() for tag in pos_tag(word_tokenize(phrase)) if tag[1] in ["NN","NNP","NNS", "NNPS", "NP"]]))
+	tags.append((phrase, "TextBlob", [tag[0].lower() for tag in textblob.TextBlob(phrase).tags if tag[1] in ["NN","NNP","NNS", "NNPS", "NP"]]))
+	done += 1
+	print("%.1f %%" % (100*done/len(text)))
+
+with open("tags.json", 'wb+') as f:
+	for tag in tags:
+		f.write(json.dumps(tag)+'\n')
+
+nltk_nouns = {}
+textblob_nouns = {}
+
+print("Grouping tags")
+for tag in tags:
+	if "I eat " in tag[0]:
+		key = tag[0][6:-1].lower()
+	elif " tastes good." in tag[0]:
+		key = tag[0][:-13].lower()
+	else:
+		key = tag[0].lower()
+	if tag[1] == "NLTK":
+		if key not in nltk_nouns:
+			nltk_nouns[key] = set()
+		nltk_nouns[key].update(tag[2])
+	else:
+		if key not in textblob_nouns:
+			textblob_nouns[key] = set()
+		textblob_nouns[key].update(tag[2])
+
+print("Adding tags")
+done = 0.0
+for ingredient in nltk_nouns:
+	try:
+		usda[translation[ingredient]]["name"]["split"] = list(nltk_nouns[ingredient].union(textblob_nouns[ingredient]))
+	except:
+		print(ingredient)
+		raw_input()
+		try:
+			print(translation[ingredient])
+		except:
+			print(translation)
+
+
+
+print("Saving USDA")
+with open("nounUSDA.json", 'wb+') as f:
+	for ingredient_name, ingredient in usda.iteritems():
+		f.write(json.dumps(ingredient)+'\n')
+
